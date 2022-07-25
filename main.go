45f72826ac109f8d5e7e04680eea86505e1eed40
Caleb Case <calebcase@gmail.com>
2022-07-25 13:35:54 -0400

    Blurbs

--- main.go
+++ main.go
@@ -4,53 +4,74 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"syscall"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
-//TODO: Make these configurable.
-var (
-	// SEP is the separator between subcommand names.
-	SEP = "_"
+type Config struct {
+	Separator     string `env:"META_SEP" env-default:"_"`
+	SubCommandMax int    `env:"META_SUBCMD_MAX" env-default:"1"`
+}
 
-	// SUB_MAX is the maximum number of levels to consider when outputing
-	// subcommands. It defaults to 1 (or just the immediate subcommands).
-	SUB_MAX = 1
-)
+var cfg Config
 
 type Name string
 
 func (name Name) Encode() string {
-	return strings.ReplaceAll(string(name), " ", SEP)
+	return strings.ReplaceAll(string(name), " ", cfg.Separator)
 }
 
 func (name Name) Decode() string {
-	return strings.ReplaceAll(string(name), SEP, " ")
+	return strings.ReplaceAll(string(name), cfg.Separator, " ")
 }
 
 func (name Name) Parts() []string {
 	return strings.Split(name.Decode(), " ")
 }
 
-func GetSubCommands(prefix string) map[string]string {
+type Cmd struct {
+	Path  string
+	Name  string
+	Blurb string
+}
+
+func GetBlurb(path string) string {
+	output, err := exec.Command(path, "--help-blurb").Output()
+	if err != nil {
+		return ""
+	}
+
+	blurb, _, _ := strings.Cut(string(output), "\n")
+
+	return blurb
+}
+
+func GetSubCommands(prefix string) map[string]Cmd {
 	prefix = Name(prefix).Encode()
-	commands := map[string]string{}
+	commands := map[string]Cmd{}
 
 	paths := filepath.SplitList(os.Getenv("PATH"))
 	for _, path := range paths {
-		matches, err := filepath.Glob(filepath.Join(path, prefix+SEP+"*"))
+		matches, err := filepath.Glob(filepath.Join(path, prefix+cfg.Separator+"*"))
 		if err != nil {
 			continue
 		}
 
 		for _, match := range matches {
 			base := filepath.Base(match)
-			subbase := base[len(prefix+SEP):]
+			subbase := base[len(prefix+cfg.Separator):]
 			subparts := Name(subbase).Parts()
-			if len(subparts) <= SUB_MAX {
+			if len(subparts) <= cfg.SubCommandMax {
 				if _, ok := commands[subbase]; !ok {
-					commands[subbase] = match
+					commands[subbase] = Cmd{
+						Path:  match,
+						Name:  subbase,
+						Blurb: GetBlurb(match),
+					}
 				}
 			}
 		}
@@ -64,13 +85,13 @@ func GenUsage(prefix string) (usage []string) {
 		fmt.Sprintf("Usage: %s COMMAND", Name(prefix).Decode()),
 	}
 
-	commands := GetSubCommands(prefix)
+	cmds := GetSubCommands(prefix)
 
-	if len(commands) > 0 {
+	if len(cmds) > 0 {
 		usage = append(usage, "", "Commands:")
 
-		for command := range commands {
-			usage = append(usage, fmt.Sprintf("  %s %s", Name(prefix).Decode(), Name(command).Decode()))
+		for subbase, cmd := range cmds {
+			usage = append(usage, fmt.Sprintf("  %s %s\t%s", Name(prefix).Decode(), Name(subbase).Decode(), cmd.Blurb))
 		}
 	}
 
@@ -109,7 +130,7 @@ func Execute() (err error) {
 
 			args = append(os.Args[:position], os.Args[position+1:]...)
 
-			if found >= SUB_MAX {
+			if found >= cfg.SubCommandMax {
 				break
 			}
 		}
@@ -128,19 +149,27 @@ func Execute() (err error) {
 		}
 
 		subcommands := GetSubCommands(name)
-		executable, ok := subcommands[strings.Join(subcommand, SEP)]
+		cmd, ok := subcommands[strings.Join(subcommand, cfg.Separator)]
 		if !ok {
 			return errors.New("command not found")
 		}
 
-		return syscall.Exec(executable, args, os.Environ())
+		return syscall.Exec(cmd.Path, args, os.Environ())
 	}
 
 	return nil
 }
 
 func main() {
-	err := Execute()
+	var err error
+
+	err = cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\n%s: %s\n", os.Args[0], err)
+		os.Exit(1)
+	}
+
+	err = Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "\n%s: %s\n", os.Args[0], err)
 		os.Exit(1)
